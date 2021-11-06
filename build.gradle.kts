a738aa16490fa9bdff0e9e13630e5430ba0cbc17
doomsdayrs <doomsdayrs@gmail.com>
2021-11-05 23:12:43 -0400

    Fix issue with compiler java versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,40 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.31"
-    application
+	kotlin("jvm") version "1.5.31"
+	application
 }
 
 group = "com.github.doomsdayrs.lib"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
+	mavenCentral()
+	maven("https://jitpack.io")
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+	testImplementation(kotlin("test"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
-    implementation("com.github.shosetsuorg:kotlin-lib:v1.0.0-rc62")
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("com.squareup.okhttp3:okhttp:4.9.2")
-    implementation("org.luaj:luaj-jse:3.0.1")
+	implementation("com.github.shosetsuorg:kotlin-lib:v1.0.0-rc62")
+	implementation(kotlin("stdlib"))
+	implementation(kotlin("stdlib-jdk8"))
+	implementation("com.squareup.okhttp3:okhttp:4.9.2")
+	implementation("org.luaj:luaj-jse:3.0.1")
 
-    implementation(kotlin("reflect"))
+	implementation(kotlin("reflect"))
 }
 
 tasks.test {
-    useJUnit()
-}
-
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+	useJUnit()
 }
 
 application {
-    mainClass.set("TestKt")
+	mainClass.set("TestKt")
 }
